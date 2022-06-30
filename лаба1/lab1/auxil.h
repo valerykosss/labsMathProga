c3d4ccbbbdbaf712cbf3c2c965bf530b9aaa953a
valerykosss <90275681+valerykosss@users.noreply.github.com>
2022-06-30 05:41:54 +0300

    Add files via upload

---
+++ лаба1/lab1/auxil.h
@@ -0,0 +1,7 @@
+#pragma once
+namespace auxil
+{
+	void   start();                         // старт  генератора сл. чисел
+	double dget(double rmin, double rmax); // получить случайное число 
+	int    iget(int rmin, int rmax);        // получить  случайное число
+};
---
+++ лаба2/2 лаба мат прога/Lab_2_mp/Main.cpp
@@ -0,0 +1,83 @@
+////// Main 
+//#include "Auxil.h"
+//#include <iostream>
+//#include "Combi1.h"
+//#include <tchar.h>
+//#include "Knapsack.h"
+//#define NN 18  //количество предметов Ц 18 шт
+//int main(int argc, _TCHAR* argv[])
+//{
+//	setlocale(LC_ALL, "rus");
+//	int V = 300;  // вместимость рюкзака 300 кг
+//	auxil::start();
+//	int v[NN], c[NN];
+//		for (int i = 0; i < NN; i++) //заполнение рандомными числами 
+//		{
+//			v[i] = auxil::iget(10, 300); //веса предметов 10 Ц 300 кг
+//			c[i] = auxil::iget(5, 55); //стоимость предметов 5 Ц 55 у.е
+//		}	
+//	short m[NN];   // количество предметов каждого типа  {0,1}   
+//	int maxcc = knapsack_s(
+//		V,   // [in]  вместимость рюкзака 
+//		NN,  // [in]  количество типов предметов 
+//		v,   // [in]  размер предмета каждого типа  
+//		c,   // [in]  стоимость предмета каждого типа     
+//		m    // [out] количество предметов каждого типа  
+//	);
+//	std::cout << std::endl << "-------- «адача о рюкзаке ---------";
+//	std::cout << std::endl << "- количество предметов : " << NN;
+//	std::cout << std::endl << "- вместимость рюкзака  : " << V;
+//	std::cout << std::endl << "- размеры предметов    : ";
+//	for (int i = 0; i < NN; i++) std::cout << v[i] << " ";
+//	std::cout << std::endl << "- стоимости предмета   : ";
+//	for (int i = 0; i < NN; i++) std::cout << c[i] << " ";
+//	std::cout << std::endl << "- стоимости предметов  : ";
+//	for (int i = 0; i < NN; i++) std::cout << v[i] * c[i] << " ";
+//	std::cout << std::endl << "- оптимальна€ стоимость рюкзака: " << maxcc;
+//	std::cout << std::endl << "- вес рюкзака: ";
+//	int s = 0; for (int i = 0; i < NN; i++) s += m[i] * v[i];
+//	std::cout << s;
+//	std::cout << std::endl << "- выбраны предметы: ";
+//	for (int i = 0; i < NN; i++) std::cout << " " << m[i];
+//	std::cout << std::endl << std::endl;
+//	system("pause");
+//	return 0;
+//}
+
+
+
+
+#include <iostream>
+#include "Combi1.h"
+#include "Knapsack.h"
+#include <time.h>
+#include <tchar.h>
+#include <iomanip> 
+#define NN 20
+int main(int argc, _TCHAR* argv[])
+{
+	setlocale(LC_ALL, "rus");
+	int V = 300,              // вместимость рюкзака              
+		v[] = { 25, 56, 67, 40, 20, 27, 37, 33, 33, 44, 53, 12,
+			   60, 75, 12, 55, 54, 42, 43, 14, 30, 37, 31, 12 },
+		c[] = { 15, 26, 27, 43, 16, 26, 42, 22, 34, 12, 33, 30,
+			   12, 45, 60, 41, 33, 11, 14, 12, 25, 41, 30, 40 };
+	short m[NN];
+	int maxcc = 0;
+	clock_t t1, t2;
+	std::cout << std::endl << "-------- «адача о рюкзаке ---------";
+	std::cout << std::endl << "- вместимость рюкзака  : " << V;
+	std::cout << std::endl << "-- количество ------ продолжительность -- ";
+	std::cout << std::endl << "    предметов          вычислени€  ";
+	for (int i = 12; i <= NN; i++)
+	{
+		t1 = clock();
+		maxcc = knapsack_s(V, i, v, c, m);
+		t2 = clock();
+		std::cout << std::endl << "       " << std::setw(2) << i
+			<< "               " << std::setw(5) << (t2 - t1);
+	}
+	std::cout << std::endl << std::endl;
+	system("pause");
+	return 0;
+}
